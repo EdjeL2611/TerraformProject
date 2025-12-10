ee22803e372a460d8dd748178ba422ed76c36ed3
Edjel2611 <edjetestmiddelland@outlook.com>
2025-12-10 14:25:12 +0100

    configured containerapp

--- containerapp.tf
+++ containerapp.tf
@@ -1,6 +1,5 @@
 resource "azurerm_container_app" "containerapp" {
   name                         = "containerapp-${random_string.suffix.result}"
-  location                     = azurerm_resource_group.TerraformProject.location
   resource_group_name          = azurerm_resource_group.TerraformProject.name
   container_app_environment_id = azurerm_container_app_environment.appenv.id
   revision_mode                = "Single"
@@ -13,19 +12,30 @@ resource "azurerm_container_app" "containerapp" {
       memory = "1.0Gi"
     }
   }
+}
 
-  scale {
-    min_replicas = 1
-    max_replicas = 3
+resource "azapi_update_resource" "containerapp_scale" {
+  type        = "Microsoft.App/containerApps@2024-03-01"
+  resource_id = azurerm_container_app.containerapp.id
 
-    rule {
-      name = "http-rule"
-
-      http {
-        metadata = {
-          concurrentRequests = "50"
+  body = jsonencode({
+    properties = {
+      template = {
+        scale = {
+          minReplicas = 1
+          maxReplicas = 3
+          rules = [
+            {
+              name = "http-rule"
+              http = {
+                metadata = {
+                  concurrentRequests = "50"
+                }
+              }
+            }
+          ]
         }
       }
     }
-  }
+  })
 }
